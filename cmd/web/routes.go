9c6a9e17679f0ee2d4570cd0d8d4824234d7e34d
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-08-15 22:22:23 +0300

    web/routes: Move POST routes to an `/api` endpoint

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,12 +13,19 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(SessionLoad)
+	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
 	// mux.Use(NoSurf)
 
 	mux.Get("/", http.HandlerFunc(handler.Repo.HandleHomePage))
 	mux.Get("/adauga-editie", http.HandlerFunc(handler.Repo.HandleAddEditie))
-	mux.Post("/adauga-editie-json", http.HandlerFunc(handler.Repo.HandleAdaugaEditieJson))
+
+	apiRouter := chi.NewRouter()
+	apiRouter.Use(middleware.Logger)
+	apiRouter.Use(middleware.Recoverer)
+	apiRouter.Use(middleware.NoCache)
+	apiRouter.Post("/adauga-editie-json", http.HandlerFunc(handler.Repo.HandleAdaugaEditieJson))
+	mux.Mount("/api", apiRouter)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
